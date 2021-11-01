88df5b1bd65e669bf3c736cf87f73d0816f1b248
chengyanwu <cheng-yan@umail.ucsb.edu>
2021-11-01 10:44:52 -0700

    IW, added digits recognition program (ML result incorrect)

---
+++ workspace/rps-demo/build/pb.d
@@ -0,0 +1,24 @@
+/Users/ianwu/Desktop/Maxim-Parking/MAX78000_SDK-09056510448d101b893d84c8d89cac098a957ca2/Examples/MAX78000/CNN/rps-demo/build/pb.o: \
+ ../../../../Libraries/Boards/MAX78000/EvKit_V1/../Source/pb.c \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_device.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/max78000.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/features.h \
+ /Applications/ARM/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../Libraries/CMSIS/Include/core_cm4.h \
+ ../../../../Libraries/CMSIS/Include/core_cmInstr.h \
+ ../../../../Libraries/CMSIS/Include/core_cmFunc.h \
+ ../../../../Libraries/CMSIS/Include/core_cm4_simd.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/system_max78000.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_errors.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_pins.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/gpio.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/gpio_regs.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_assert.h \
+ ../../../../Libraries/Boards/MAX78000/EvKit_V1/../Include/pb.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/gpio.h
---
+++ workspace/rps-demo/build/startup_max78000.d
@@ -0,0 +1,2 @@
+/Users/ianwu/Desktop/Maxim-Parking/MAX78000_SDK-09056510448d101b893d84c8d89cac098a957ca2/Examples/MAX78000/CNN/rps-demo/build/startup_max78000.o: \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Source/GCC/startup_max78000.S
