33488314f20101ef8347e9b227223f4e8b11afec
chengyanwu <cheng-yan@umail.ucsb.edu>
2021-11-04 17:06:04 -0700

    IW, added featherboard digit reg(CNN_Start failed)

---
+++ workspace/digit-recognition-FTHR/build/max20303.d
@@ -0,0 +1,28 @@
+/Users/ianwu/Desktop/Maxim-Parking/MAX78000_SDK-09056510448d101b893d84c8d89cac098a957ca2/Examples/MAX78000/CNN/rps-demo/build/max20303.o: \
+ ../../../../Libraries/Boards/MAX78000/FTHR_RevA/../Source/max20303.c \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/features.h \
+ /Applications/ARM/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_stdint.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_device.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/max78000.h \
+ ../../../../Libraries/CMSIS/Include/core_cm4.h \
+ ../../../../Libraries/CMSIS/Include/core_cmInstr.h \
+ ../../../../Libraries/CMSIS/Include/core_cmFunc.h \
+ ../../../../Libraries/CMSIS/Include/core_cm4_simd.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/system_max78000.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_errors.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_pins.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/gpio.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/gpio_regs.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_delay.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/i2c.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_sys.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_device.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/lpgcr_regs.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/gcr_regs.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/i2c_regs.h
