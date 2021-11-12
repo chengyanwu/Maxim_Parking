13df9f3174f98669dc0e3b5e43c198f3aaa684a2
chengyanwu <cheng-yan@umail.ucsb.edu>
2021-11-12 15:43:41 -0800

    IW, added car-detection code

---
+++ workspace/car_detection/build/all_imgs.d
@@ -0,0 +1,2 @@
+/Users/ianwu/Desktop/Maxim-Parking/MAX78000_SDK-09056510448d101b893d84c8d89cac098a957ca2/Examples/MAX78000/CNN/car_detection/build/all_imgs.o: \
+ TFT/all_imgs.c
---
+++ workspace/car_detection/build/heap.d
@@ -0,0 +1,34 @@
+/Users/ianwu/Desktop/Maxim-Parking/MAX78000_SDK-09056510448d101b893d84c8d89cac098a957ca2/Examples/MAX78000/CNN/car_detection/build/heap.o: \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Source/heap.c \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/features.h \
+ /Applications/ARM/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_stdint.h \
+ /Applications/ARM/arm-none-eabi/include/errno.h \
+ /Applications/ARM/arm-none-eabi/include/sys/errno.h \
+ /Applications/ARM/arm-none-eabi/include/sys/reent.h \
+ /Applications/ARM/arm-none-eabi/include/_ansi.h \
+ /Applications/ARM/arm-none-eabi/include/newlib.h \
+ /Applications/ARM/arm-none-eabi/include/sys/config.h \
+ /Applications/ARM/arm-none-eabi/include/machine/ieeefp.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_types.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/lock.h \
+ /Applications/ARM/arm-none-eabi/include/unistd.h \
+ /Applications/ARM/arm-none-eabi/include/sys/unistd.h \
+ /Applications/ARM/arm-none-eabi/include/sys/cdefs.h \
+ /Applications/ARM/arm-none-eabi/include/sys/types.h \
+ /Applications/ARM/arm-none-eabi/include/machine/endian.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_endian.h \
+ /Applications/ARM/arm-none-eabi/include/sys/select.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_sigset.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_timeval.h \
+ /Applications/ARM/arm-none-eabi/include/sys/timespec.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_timespec.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /Applications/ARM/arm-none-eabi/include/sys/sched.h \
+ /Applications/ARM/arm-none-eabi/include/machine/types.h
