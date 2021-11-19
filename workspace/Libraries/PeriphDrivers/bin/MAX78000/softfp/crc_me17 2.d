f592db9529a1cc1bc0460bb8b7b15a2b73bfcba2
chengyanwu <cheng-yan@umail.ucsb.edu>
2021-11-19 13:49:26 -0800

    add in libraries

---
+++ workspace/Libraries/PeriphDrivers/bin/MAX78000/softfp/crc_me17 2.d
@@ -0,0 +1,30 @@
+../../../../Libraries/PeriphDrivers/bin/MAX78000/softfp/crc_me17.o: \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Source/CRC/crc_me17.c \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_device.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/max78000.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/features.h \
+ /Applications/ARM/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../Libraries/CMSIS/Include/core_cm4.h \
+ ../../../../Libraries/CMSIS/Include/core_cmInstr.h \
+ ../../../../Libraries/CMSIS/Include/core_cmFunc.h \
+ ../../../../Libraries/CMSIS/Include/core_cm4_simd.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/system_max78000.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_errors.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_pins.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/gpio.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/gpio_regs.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_errors.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_assert.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_sys.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_device.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/lpgcr_regs.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/gcr_regs.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/crc.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/crc_regs.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Source/CRC/crc_reva.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Source/CRC/crc_reva_regs.h
---
+++ workspace/Libraries/PeriphDrivers/bin/MAX78000/softfp/uart_ai85.d
@@ -0,0 +1,34 @@
+../../../../Libraries/PeriphDrivers/bin/MAX78000/softfp/uart_ai85.o: \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Source/UART/uart_ai85.c \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/uart.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/uart_regs.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/features.h \
+ /Applications/ARM/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_sys.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_device.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/max78000.h \
+ ../../../../Libraries/CMSIS/Include/core_cm4.h \
+ ../../../../Libraries/CMSIS/Include/core_cmInstr.h \
+ ../../../../Libraries/CMSIS/Include/core_cmFunc.h \
+ ../../../../Libraries/CMSIS/Include/core_cm4_simd.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/system_max78000.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_errors.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_pins.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/gpio.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/gpio_regs.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/lpgcr_regs.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/gcr_regs.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_device.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_pins.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/mxc_assert.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Source/UART/uart_revb.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Include/MAX78000/dma.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/dma_regs.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Source/UART/uart_revb_regs.h \
+ ../../../../Libraries/CMSIS/../PeriphDrivers/Source/UART/uart_common.h
