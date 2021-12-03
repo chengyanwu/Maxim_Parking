ea35b6eefd435ae0f0a44159e031b8d3178d4e90
Kyle Wong <kylewong@169-231-151-146.wireless.ucsb.edu>
2021-12-03 13:55:49 -0800

    added cats-dogs_demo

---
+++ workspace/Examples/MAX78000/CNN/cats-dogs_demo/build/SansSerif16x16.d
@@ -0,0 +1,2 @@
+/Users/kylewong/Documents/School/ece189/ai8x-synthesis/sdk/Examples/MAX78000/CNN/cats-dogs_demo/build/SansSerif16x16.o: \
+ TFT/fthr/SansSerif16x16.c
---
+++ workspace/Examples/MAX78000/CNN/cats-dogs_demo/build/logo_rgb565.d
@@ -0,0 +1,2 @@
+/Users/kylewong/Documents/School/ece189/ai8x-synthesis/sdk/Examples/MAX78000/CNN/cats-dogs_demo/build/logo_rgb565.o: \
+ TFT/fthr/logo_rgb565.c
