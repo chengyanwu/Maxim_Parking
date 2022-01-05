c7b868d5e430508e3dba5b2a296508490ca8b9c0
hm85451 <43687967+hm85451@users.noreply.github.com>
2022-01-04 17:22:49 -0800

    build for cameraIF

---
+++ workspace/Examples/MAX78000/CNN/CameraIF/build/Fat32Driver/ffsystem.d
@@ -0,0 +1,2 @@
+c:/Users/hmche/Desktop/capstone/Maxim_Parking/workspace/Examples/MAX78000/CNN/CameraIF/build/Fat32Driver/ffsystem.o: \
+ Source/ffsystem.c Source/ff.h Source/integer.h Source/ffconf.h
