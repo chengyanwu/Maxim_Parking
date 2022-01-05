1fdbe9a5ff50f1bb843458b5748146072d98e456
hm85451 <43687967+hm85451@users.noreply.github.com>
2022-01-05 15:05:43 -0800

    added cameraIF builds

--- workspace/Examples/MAX78000/CNN/CameraIF/main.c
+++ workspace/Examples/MAX78000/CNN/CameraIF/main.c
@@ -67,8 +67,8 @@
 #include "ff.h"
 #include "sd.h"
 
-#define IMAGE_XRES  128
-#define IMAGE_YRES  64
+#define IMAGE_XRES  250
+#define IMAGE_YRES  200
 #define CAMERA_FREQ (10 * 1000 * 1000)
 
 #define STRINGIFY(x) #x
