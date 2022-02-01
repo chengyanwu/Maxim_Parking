a77838c6c2aa1cfdc8091906ec2e0f2b1774da74
chengyanwu <cheng-yan@ucsb.edu>
2022-02-01 13:43:10 -0800

    add LoRa with 565seg

---
+++ workspace/Examples/MAX78000/CNN/Car_Detection_w_LoRaSeg4/build/heap.d
@@ -0,0 +1,34 @@
+/Users/ianwu/Desktop/SpotOn/Maxim_Parking/workspace/Examples/MAX78000/CNN/Car_Detection_w_LoRaSeg4/build/heap.o: \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Source/heap.c \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/errno.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/errno.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/unistd.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/unistd.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/types.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/endian.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_endian.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/select.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_sigset.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_timeval.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/timespec.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_timespec.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/sched.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/types.h
