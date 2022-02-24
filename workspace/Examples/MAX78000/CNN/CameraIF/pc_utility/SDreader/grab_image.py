b35ebfb88d84e815a609c648f47a12737514d2ce
hm85451 <43687967+hm85451@users.noreply.github.com>
2022-02-24 15:49:03 -0800

    save model input images and results

--- workspace/Examples/MAX78000/CNN/CameraIF/pc_utility/SDreader/grab_image.py
+++ workspace/Examples/MAX78000/CNN/CameraIF/pc_utility/SDreader/grab_image.py
@@ -5,15 +5,15 @@ import sys
 DIR = './rawData'
 numOfFiles = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
 
-for i in range(1,5):
-    image = bytearray()
-    f = open('./rawData/image' + str(i), "rb")
-    image+=f.read()
-    f.close()
-    outputFileName = "./image/image"+str(i)+".png" 
-    imgConverter.convert(image, outputFileName, int(sys.argv[1]), int(sys.argv[2]), "RGB565")
+# for i in range(0,2):
+#     image = bytearray()
+#     f = open('./rawData/image' + str(i), "rb")
+#     image+=f.read()
+#     f.close()
+#     outputFileName = "./image/image"+str(i)+".png" 
+#     imgConverter.convert(image, outputFileName, int(sys.argv[1]), int(sys.argv[2]), "RGB565")
     
-for i in range(1001,1005):
+for i in range(0,5):
     image = bytearray()
     f = open('./rawData/image' + str(i), "rb")
     image+=f.read()
