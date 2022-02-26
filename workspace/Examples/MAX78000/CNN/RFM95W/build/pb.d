ad0d30f4654423f7aa371c50fded650b06661eb6
chengyanwu <cheng-yan@ucsb.edu>
2022-02-25 17:22:32 -0800

    added rfm95w example

---
+++ workspace/Examples/MAX78000/CNN/RFM95W/build/pb.d
@@ -0,0 +1,24 @@
+/Users/ianwu/Desktop/SpotOn/Maxim_Parking/workspace/Examples/MAX78000/CNN/RFM95W/build/pb.o: \
+ ../../../../Libraries/Boards/MAX78000/FTHR_RevA/../Source/pb.c \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_device.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/max78000.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../Libraries/CMSIS/Include/core_cm4.h \
+ ../../../../Libraries/CMSIS/Include/core_cmInstr.h \
+ ../../../../Libraries/CMSIS/Include/core_cmFunc.h \
+ ../../../../Libraries/CMSIS/Include/core_cm4_simd.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/system_max78000.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_errors.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_pins.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/gpio.h \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Include/gpio_regs.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/mxc_assert.h \
+ ../../../../Libraries/Boards/MAX78000/FTHR_RevA/../Include/pb.h \
+ ../../../../Libraries/PeriphDrivers/Include/MAX78000/gpio.h
---
+++ workspace/Examples/MAX78000/CNN/RFM95W/build/startup_max78000.d
@@ -0,0 +1,2 @@
+/Users/ianwu/Desktop/SpotOn/Maxim_Parking/workspace/Examples/MAX78000/CNN/RFM95W/build/startup_max78000.o: \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Source/GCC/startup_max78000.S
