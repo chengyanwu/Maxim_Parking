5220c79a3861a8e7c69ac50374729e34d1e2fb15
hm85451 <43687967+hm85451@users.noreply.github.com>
2022-05-07 18:21:28 -0700

    changed cnn load input

--- workspace/Examples/MAX78000/CNN/car_detection/lmic/hal.c
+++ workspace/Examples/MAX78000/CNN/car_detection/lmic/hal.c
@@ -51,7 +51,7 @@
 #define DATA_LEN        8         // Words
 #define DATA_VALUE      0xA5A5      // This is for master mode only...
 #define VALUE           0xFFFF
-#define SPI_SPEED       500000      // Bit Rate
+#define SPI_SPEED       5000000      // Bit Rate
 
 #define SPI_INSTANCE_NUM    1
 
